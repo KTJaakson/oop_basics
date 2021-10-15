3af2738656c9fe4feea24bb3aed7bf7b9d7f8172
KTJaakson <karl-tauno.jaakson@khk.ee>
2021-10-15 11:32:18 +0300

    test1-katse5

--- app.js
+++ app.js
@@ -52,26 +52,7 @@ square.calcPerimeter();
 var triangle = new Shape('triangle', 3, 3);
 triangle.calcPerimeter();
 
-
-class Shape extends Object {
-  constructor(name, sides, sideLength){
-    super();
-    this.name = name;
-    this.sides = sides;
-    this.sideLength = sideLength;
-  }
-  calcPerimeter() {
-    console.log(this.sides * this.sideLength);
-  }
-}
-
-let square = new Shape('es6Square', 4, 5);
-square.calcPerimeter();
-
-let triangle = new Shape('es6Triangle', 3, 3);
-triangle.calcPerimeter();
-
-*/
+////////
 
 class Shape extends Object {
   constructor(name, sides, sideLength){
@@ -90,3 +71,28 @@ square.calcPerimeter();
 
 let triangle = new Shape('Triangle', 3, 3);
 triangle.calcPerimeter();
+
+*/
+
+class Shape {
+  constructor(name, sides, sideLength){
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+  calcPerimeter() {
+    console.log(this.sides * this.sideLength);
+  }
+}
+
+class Square extends Shape {
+  constructor(name, sideLength) {
+    super(name, 4, sideLength);
+  }
+  calcArea() {
+    console.log(this.sideLength * this.sideLength);
+  }
+}
+
+let square = new Square('newclassSquare', 5);
+square.calcPerimeter();
